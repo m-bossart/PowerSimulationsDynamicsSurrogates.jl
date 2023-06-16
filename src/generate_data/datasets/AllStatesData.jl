868782f15164d2ae1d34fc5e956f5febd0f415e5
Matt Bossart <bossart.matt@gmail.com>
2023-06-16 13:04:44 +0200

    add references to AllStatesData

--- src/generate_data/datasets/AllStatesData.jl
+++ src/generate_data/datasets/AllStatesData.jl
@@ -4,7 +4,8 @@ mutable struct AllStatesData <: SurrogateDataset
     tstops::AbstractArray
     stable::Bool
     solve_time::Float64
-    device_states_data::Dict{String, Dict{Symbol, AbstractArray}} #string is device name, "symbol is :vr, :vi, :ir, :ii, arrays are the data
+    device_states_data::Dict{String, Dict{Symbol, AbstractArray}} 
+    device_ref_data::Dict{String, Dict{Symbol, Float64}} 
 end
 
 function AllStatesData(;
@@ -14,8 +15,9 @@ function AllStatesData(;
     stable = false,
     solve_time = 0.0,
     device_states_data = Dict{String, Dict{Symbol, AbstractArray}}(),
+    device_ref_data = Dict{String, Dict{Symbol, Float64}}(),
 )
-    return AllStatesData(type, tsteps, tstops, stable, solve_time, device_states_data)
+    return AllStatesData(type, tsteps, tstops, stable, solve_time, device_states_data, device_ref_data)
 end
 
 function fill_surrogate_data!(
@@ -28,6 +30,7 @@ function fill_surrogate_data!(
 )
     sys = sim_full.sys
     states_data_dict = Dict{String, Dict{Symbol, AbstractArray}}()
+    references_data_dict = Dict{String, Dict{Symbol, Float64}}()
     for (device_name, orientation_details) in device_details
         all_components_with_name =
             PSY.get_components_by_name(PSY.Component, sys, device_name)
@@ -44,12 +47,22 @@ function fill_surrogate_data!(
             data_collection_params,
             sys,
         )
+        _fill_references_data!(
+            references_data_dict,
+            results,
+            device,
+            save_indices,
+            orientation_details,
+            data_collection_params,
+            sys,
+        )
     end
     data.tstops = unique(results.solution.t)
     data.tsteps = unique(results.solution.t)[save_indices]
     data.stable = true
     data.solve_time = results.time_log[:timed_solve_time]
     data.device_states_data = states_data_dict
+    data.device_ref_data = references_data_dict
 end
 
 function _fill_states_data!(
@@ -70,6 +83,29 @@ function _fill_states_data!(
             PSID.get_state_series(results, (dynamic_device_name, s))[2][save_indices]
     end
     states_data_dict[dynamic_device_name] = state_trajectories_dict
+end
+
+function _fill_references_data!(
+    refs_data_dict,
+    results,
+    device::S,
+    save_indices,
+    orientation_details,
+    data_collection_params,
+    sys,
+) where {S <: PSY.StaticInjection}
+    references = Dict{Symbol, Float64}()
+    dynamic_device_name = PSY.get_name(device)
+    dynamic_device = PSY.get_dynamic_injector(device)
+    @assert dynamic_device !== nothing
+    references[:P_ref] = PSY.get_P_ref(dynamic_device)
+    references[:ω_ref] = 1.0
+    references[:V_ref] = PSY.get_V_ref(dynamic_device)
+    references[:Q_ref] = PSY.get_reactive_power(device)
+    if typeof(dynamic_device) <: PSY.DynamicGenerator
+        references[:eq_p] = PSY.get_eq_p(PSY.get_machine(dynamic_device))
+    end 
+    refs_data_dict[dynamic_device_name] = references
 end
 
 function EmptyTrainDataSet(T::Type{AllStatesData})
