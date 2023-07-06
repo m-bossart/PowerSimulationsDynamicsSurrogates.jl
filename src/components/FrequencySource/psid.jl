542f3de4242e58c3b6a40bba1405b54693383d0d
Matt Bossart <bossart.matt@gmail.com>
2023-07-06 10:25:14 +0200

    commit untracked files (incomplete)

---
+++ src/components/FrequencySource/psid.jl
@@ -0,0 +1,149 @@
+PSID.is_valid(::FrequencySource) = nothing
+
+function PSID._get_frequency_state(d::PSID.DynamicWrapper{FrequencySource})
+    return PSID.get_global_index(d)[:ω]
+end
+
+function PSID.device_mass_matrix_entries!(
+    mass_matrix::PSID.AbstractArray,
+    dynamic_device::PSID.DynamicWrapper{FrequencySource},
+)
+    global_index = PSID.get_global_index(dynamic_device)
+    mass_matrix_device_entries!(mass_matrix, dynamic_device, global_index)
+    return
+end
+
+function mass_matrix_device_entries!(
+    mass_matrix,
+    device::PSID.DynamicWrapper{FrequencySource},
+    global_index::Base.ImmutableDict{Symbol, Int64},
+)
+    mass_matrix[global_index[:Vt], global_index[:Vt]] = PSY.get_Tv(PSID.get_device(device))
+    mass_matrix[global_index[:ω], global_index[:ω]] = get_Tω(PSID.get_device(device))
+    mass_matrix[global_index[:θt], global_index[:θt]] =  1/(2*pi*60)
+    return
+end 
+
+
+function PSID.device!(
+    device_states::AbstractArray{T},
+    output_ode::AbstractArray{T},
+    voltage_r::T,
+    voltage_i::T,
+    current_r::AbstractArray{T},
+    current_i::AbstractArray{T},
+    ::AbstractArray{T},
+    ::AbstractArray{T},
+    dynamic_device::PSID.DynamicWrapper{FrequencySource},
+    t,
+) where {T <: PSID.ACCEPTED_REAL_TYPES}
+
+    # Internal Voltage states
+    Vt = device_states[1]
+    θt = device_states[2]
+    ω = device_states[3]
+
+    V_R = Vt * cos(θt)
+    V_I = Vt * sin(θt)
+    
+    V_ref = PSID.get_V_ref(dynamic_device)
+    ω_ref = PSID.get_ω_ref(dynamic_device) 
+
+    Tv = PSY.get_Tv(PSID.get_device(dynamic_device))
+    Tω = get_Tω(PSID.get_device(dynamic_device))
+
+    _, dVt_dt =  PSID.low_pass_mass_matrix(V_ref, Vt, 1.0, Tv)
+    _, dω_dt =  PSID.low_pass_mass_matrix(ω_ref, ω, 1.0, Tω)
+    _, dθt_dt = PSID.integrator_nonwindup_mass_matrix(ω-1.0, θt, 1.0, 1/(2*pi*60), -Inf, Inf)
+
+    output_ode[1] = dVt_dt     
+    output_ode[2] = dθt_dt          
+    output_ode[3] = dω_dt           
+
+    #update current
+    R_th = PSY.get_R_th(PSID.get_device(dynamic_device))
+    X_th = PSY.get_X_th(PSID.get_device(dynamic_device))
+    Zmag = R_th^2 + X_th^2
+    current_r[1] += R_th * (V_R - voltage_r[1]) / Zmag + X_th * (V_I - voltage_i[1]) / Zmag #in system pu flowing out
+    current_i[1] += R_th * (V_I - voltage_i[1]) / Zmag - X_th * (V_R - voltage_r[1]) / Zmag #in system pu flowing out
+
+    return
+end
+
+function PSID.initialize_dynamic_device!(
+    dynamic_device::PSID.DynamicWrapper{FrequencySource},
+    source::PSY.Source,
+    ::AbstractVector,
+)
+    device_states = zeros(PSY.get_n_states(dynamic_device))
+
+    #PowerFlow Data
+    P0 = PSY.get_active_power(source)
+    Q0 = PSY.get_reactive_power(source)
+    Vm = PSY.get_magnitude(PSY.get_bus(source))
+    θ = PSY.get_angle(PSY.get_bus(source))
+    S0 = P0 + Q0 * 1im
+    V_R = Vm * cos(θ)
+    V_I = Vm * sin(θ)
+    V = V_R + V_I * 1im
+    I = conj(S0 / V)
+    I_R = real(I)
+    I_I = imag(I)
+    R_th = PSY.get_R_th(source)
+    X_th = PSY.get_X_th(source)
+    Zmag = R_th^2 + X_th^2
+    function f!(out, x)
+        V_R_internal = x[1]
+        V_I_internal = x[2]
+
+        out[1] =
+            R_th * (V_R_internal - V_R) / Zmag + X_th * (V_I_internal - V_I) / Zmag - I_R
+        out[2] =
+            R_th * (V_I_internal - V_I) / Zmag - X_th * (V_R_internal - V_R) / Zmag - I_I
+    end
+    x0 = [V_R, V_I]
+    sol = NLsolve.nlsolve(f!, x0)
+    if !NLsolve.converged(sol)
+        @warn("Initialization in Chirp Signal Source failed")
+    else
+        sol_x0 = sol.zero
+        #Update terminal voltages
+        V_internal = sqrt(sol_x0[1]^2 + sol_x0[2]^2)
+        θ_internal = atan(sol_x0[2], sol_x0[1])
+        device_states[1] = V_internal
+        device_states[2] = θ_internal
+        device_states[3] = 1.0
+
+        PSID.set_V_ref(dynamic_device, V_internal)
+        PSID.set_ω_ref(dynamic_device, 1.0)
+    end
+    return device_states
+end
+
+PSID.get_inner_vars_count(::FrequencySource) = 0
+
+"""
+Function to obtain the output current time series of a PeriodicVariableSource model out of the DAE Solution. It receives the simulation inputs,
+the dynamic device and bus voltage. It is dispatched for device type to compute the specific current.
+compute_output_current(::SimulationResults, ::PeriodicVariableSource, ::Vector{Float64}, ::Vector{Float64}, ::Nothing)
+"""
+function PSID.compute_output_current(
+    res::PSID.SimulationResults,
+    dynamic_device::FrequencySource,
+    V_R::Vector{Float64},
+    V_I::Vector{Float64},
+    dt::Union{Nothing, Float64},
+)
+    name = PSY.get_name(dynamic_device)
+    ts, Vt_internal = PSID.post_proc_state_series(res, (name, :Vt), dt)
+    _, θt_internal = PSID.post_proc_state_series(res, (name, :θt), dt)
+    Vr_internal = Vt_internal .* cos.(θt_internal)
+    Vi_internal = Vt_internal .* sin.(θt_internal)
+    R_th = PSY.get_R_th(dynamic_device)
+    X_th = PSY.get_X_th(dynamic_device)
+    Z_sq = R_th^2 + X_th^2
+    I_R = R_th * (Vr_internal .- V_R) / Z_sq + X_th * (Vi_internal .- V_I) / Z_sq
+    I_I = R_th * (Vi_internal .- V_I) / Z_sq - X_th * (Vr_internal .- V_R) / Z_sq
+
+    return ts, I_R, I_I
+end
---
+++ src/components/FrequencySource/psy.jl
@@ -0,0 +1,150 @@
+"""
+    mutable struct FrequencySource <: DynamicInjection
+        name::String
+        R_th::Float64
+        X_th::Float64
+        Tv::Float64
+        Tω::Float64
+        base_power::Float64
+        states::Vector{Symbol}
+        n_states::Int
+        ext::Dict{String, Any}
+        internal::InfrastructureSystemsInternal
+    end
+This struct acts as an infinity bus with time varying phasor values magnitude and angle V(t) 	heta(t). Time varying functions are represented using fourier series
+# Arguments
+- `name::String`
+- `R_th::Float64`: Source Thevenin resistance, validation range: `(0, nothing)`
+- `X_th::Float64`: Source Thevenin reactance, validation range: `(0, nothing)`
+- `Tv::Float64`: time constant for voltage low pass filter
+- `Tω::Float64`: time contstant for frequency low pass filter
+- `base_power::Float64`: Base power
+- `states::Vector{Symbol}`: State for time, voltage and angle
+- `n_states::Int`
+- `ext::Dict{String, Any}`
+- `internal::InfrastructureSystemsInternal`: power system internal reference, do not modify
+"""
+mutable struct FrequencySource <: PSY.DynamicInjection
+    name::String
+    "Source Thevenin resistance"
+    R_th::Float64
+    "Source Thevenin reactance"
+    X_th::Float64
+    "Voltage reference"
+    V_ref::Float64
+    "Frequency reference"
+    ω_ref::Float64
+    "Time constant for voltage low pass filter"
+    Tv::Float64
+    "Time constant for frequency low pass filter"
+    Tω::Float64
+    "Base power"
+    base_power::Float64
+    "State for time, voltage and angle"
+    states::Vector{Symbol}
+    n_states::Int
+    ext::Dict{String, Any}
+    "power system internal reference, do not modify"
+    internal::PSY.InfrastructureSystemsInternal
+end
+
+function FrequencySource(
+    name,
+    R_th,
+    X_th,
+    V_ref,
+    ω_ref, 
+    Tv = 1.0,
+    Tω = 1.0,
+    base_power = 100.0,
+    ext = Dict{String, Any}(),
+)
+    FrequencySource(
+        name,
+        R_th,
+        X_th,
+        V_ref,
+        ω_ref,
+        Tv,
+        Tω,
+        base_power,
+        ext,
+        [:Vt, :θt, :ω],
+        3,
+        PSY.InfrastructureSystemsInternal(),
+    )
+end
+
+function FrequencySource(;
+    name,
+    R_th,
+    X_th,
+    V_ref,
+    ω_ref,
+    Tv = 1.0,
+    Tω = 2.0,
+    base_power = 100.0,
+    states = [:Vt, :θt, :ω],
+    n_states = 3,
+    ext = Dict{String, Any}(),
+    internal = PSY.InfrastructureSystemsInternal(),
+)
+    FrequencySource(name, R_th, X_th, V_ref, ω_ref, Tv, Tω, base_power, states, n_states, ext, internal)
+end
+
+# Constructor for demo purposes; non-functional.
+function FrequencySource(::Nothing)
+    FrequencySource(;
+        name = "init",
+        R_th = 0,
+        X_th = 0,
+        V_ref = 1.0,
+        ω_ref = 1.0,
+        Tv = 1.0,
+        Tω = 1.0,
+        base_power = 0,
+        ext = Dict{String, Any}(),
+    )
+end
+
+"""Get [`FrequencySource`](@ref) `name`."""
+PSY.get_name(value::FrequencySource) = value.name
+"""Get [`FrequencySource`](@ref) `R_th`."""
+PSY.get_R_th(value::FrequencySource) = value.R_th
+"""Get [`FrequencySource`](@ref) `X_th`."""
+PSY.get_X_th(value::FrequencySource) = value.X_th
+"""Get [`FrequencySource`](@ref) `V_ref`."""
+PSY.get_V_ref(value::FrequencySource) = value.V_ref
+"""Get [`FrequencySource`](@ref) `ω_ref`."""
+PSY.get_ω_ref(value::FrequencySource) = value.ω_ref
+"""Get [`FrequencySource`](@ref) `Tv`."""
+PSY.get_Tv(value::FrequencySource) = value.Tv
+"""Get [`FrequencySource`](@ref) `Tω`."""
+get_Tω(value::FrequencySource) = value.Tω
+"""Get [`FrequencySource`](@ref) `base_power`."""
+PSY.get_base_power(value::FrequencySource) = value.base_power
+"""Get [`FrequencySource`](@ref) `states`."""
+PSY.get_states(value::FrequencySource) = value.states
+"""Get [`FrequencySource`](@ref) `n_states`."""
+PSY.get_n_states(value::FrequencySource) = value.n_states
+"""Get [`FrequencySource`](@ref) `ext`."""
+PSY.get_ext(value::FrequencySource) = value.ext
+"""Get [`FrequencySource`](@ref) `internal`."""
+PSY.get_internal(value::FrequencySource) = value.internal
+
+"""Set [`FrequencySource`](@ref) `R_th`."""
+PSY.set_R_th!(value::FrequencySource, val) = value.R_th = val
+"""Set [`FrequencySource`](@ref) `X_th`."""
+PSY.set_X_th!(value::FrequencySource, val) = value.X_th = val
+"""Set [`FrequencySource`](@ref) `V_ref`."""
+PSY.set_V_ref!(value::FrequencySource, val) = value.V_ref = val
+"""Set [`FrequencySource`](@ref) `ω_ref`."""
+PSY.set_ω_ref!(value::FrequencySource, val) = value.ω_ref = val
+"""Set [`FrequencySource`](@ref) `Tv`."""
+PSY.set_Tv!(value::FrequencySource, val) = value.Tv = val
+"""Set [`FrequencySource`](@ref) `Tω`."""
+set_Tω!(value::FrequencySource, val) = value.Tω = val
+"""Set [`FrequencySource`](@ref) `base_power`."""
+PSY.set_base_power!(value::FrequencySource, val) = value.base_power = val
+"""Set [`FrequencySource`](@ref) `ext`."""
+PSY.set_ext!(value::FrequencySource, val) = value.ext = val
