fa70e1eaa064903035c115e4a110387210eb766c
Matt Bossart <bossart.matt@gmail.com>
2023-09-06 12:39:43 -0600

    add parsing for data scalers

--- src/components/DataScaler/psid.jl
+++ src/components/DataScaler/psid.jl
@@ -1,3 +1,17 @@
+function parse_min_max(statistics_file_path, input_indices, target_indices)
+    df = DataFrames.DataFrame(CSV.File(statistics_file_path))
+    return MinMaxScaler(;
+    scale_input = true,
+    input_min = Array(df[1, input_indices]), 
+    input_max = Array(df[2, input_indices]), 
+    input_lims = (0.0, 1.0),
+    scale_target = false,
+    target_min = Array(df[1, target_indices]), 
+    target_max = Array(df[2, target_indices]), 
+    target_lims = (0.0, 1.0),
+)
+end 
+
 function min_max_normalization(x, xmin, xmax, u, l)  #https://www.baeldung.com/cs/normalizing-inputs-artificial-neural-network
     x_prime = (x .- xmin) ./ (xmax .- xmin) .* (u .- l) .+ l
     return x_prime
@@ -34,12 +48,24 @@ function _target_scale_inverse(scaler::MinMaxScaler, x)
     end
 end
 
+function parse_standard(statistics_file_path, input_indices, target_indices)
+    df = DataFrames.DataFrame(CSV.File(statistics_file_path))
+    return  StandardScaler(;
+        scale_input = true,
+        input_mean = Array(df[3, input_indices]), 
+        input_std =  Array(df[4, input_indices]), 
+        scale_target = false,
+        target_mean =  Array(df[3, target_indices]), 
+        target_std =  Array(df[4, target_indices]), 
+)
+end 
+
 function standard_normalization(x, xmean, xstd)
     x_prime = (x .- xmean) ./ xstd
     return x_prime
 end
 
-function standard_normalization_inverse(x, xmean, xstd)
+function standard_normalization_inverse(x_prime, xmean, xstd)
     x = (x_prime .* xstd) .+ xmean
     return x
 end
